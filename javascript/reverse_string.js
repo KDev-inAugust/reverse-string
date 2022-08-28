d27e1f3b6488cfcc9c846b67310e0189fa783846
Kane Mathis <admin@kanemathis.com>
2022-08-28 17:04:56 -0400

    commit tree 845671e260fba307c8c2972e1196f93c46db3506

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,18 @@
 function reverseString(str) {
-  // type your code here
+  let reverseArr=[];
+  for (x=str.length-1; x>=0; x--){  
+    reverseArr.push(str[x]);
+  }
+  reverseArr.join();
+  return (reverseArr.toString().replaceAll(',',''));
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  console.log("Expecting: 'eM esreveR'");
+  console.log("=>", reverseString("Reverse Me"));
+
+ console.log("");
+ 
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -15,5 +24,4 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+//I will iterate through this string going through each charachter as an index that is set up as 'x' in a for loop and then subtracting 1 from x each time until the for loop reaches the beginning of the string. 
